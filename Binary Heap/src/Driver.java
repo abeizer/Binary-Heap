2467687d3df1afd7e69ef59c7d617a2fc10022d4
Abby Beizer <abbybeizer@gmail.com>
2018-04-17 12:31:36 -0400

    Testing union function

--- Binary Heap/src/Driver.java
+++ Binary Heap/src/Driver.java
@@ -4,14 +4,26 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		BinaryHeap<Integer> a = new BinaryHeap<>();
+		a.insert(100);	System.out.println(a.toString());
+		a.insert(2);	System.out.println(a.toString());
+		a.insert(1);	System.out.println(a.toString());
+		
+		
+		
+		
 		BinaryHeap<Integer> b = new BinaryHeap<>();
 		
-		b.insert(5);
-		b.insert(3);
-		b.insert(4);
-		b.insert(6);
+		b.insert(8);	System.out.println(b.toString());
+		b.insert(3);	System.out.println(b.toString());
+		b.insert(4);	System.out.println(b.toString());
+		b.insert(6);	System.out.println(b.toString());
+		b.insert(7);	System.out.println(b.toString() + "\n");
+
+		b.union(a);
 		
 		System.out.println(b.toString());
+		
 
 	}
 
